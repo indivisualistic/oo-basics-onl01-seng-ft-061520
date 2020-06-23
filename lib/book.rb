8daab30942bd5b5bb5e6bbcea869e3b73daae0d0
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-23 18:18:57 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,47 +1,36 @@
 class Book
-    attr_accessor :author, :page_count, :genre
-    attr_reader :title
-​
-    Book.new("And Then There Were None")
-    def initialize(title)
-        @title = title
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+  Book.new("And Then There Were None")
 
+  def initialize(title)
+    @title = title
+  end
+  def title
+    @title
+  end
+  def author=(author)
+    @author = author
+  end
+  def author
+    @autho
+  end
+  def page_count=(num)
+    @page_count = num
+  end
+  def page_count
+    @page_count
+  end
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
     end
-​
-    def title
-        @title
-​
-    end
-​
-    def author=(author)
-         @author = author
-    end
-​
-    def author
-        @author
-    end
-​
-    def page_count=(num)
-        @page_count = num
-    end
-​
-    def page_count
-        @page_count
-    end
-​
-    def genre=(genre)
-        @genre = genre
-    end
-​
-    def genre
-        @genre
-    end
-​
-    def turn_page
-        puts "Flipping the page...wow, you read fast!"
-    end
-​
-end
+  end
 
 
 
